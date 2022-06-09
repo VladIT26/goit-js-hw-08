48fbe20084515643140f2ef9de514f2b271f5ad9
VladIT26 <vlad.ua26@gmail.com>
2022-06-10 01:51:54 +0300

    changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Vimeo from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('iframe');
+console.log(iframe)
+const player = new Vimeo(iframe);
+
+
+const onPlay = function (data) {
+duration: 61.857
+percent: 0.049
+seconds: 3.034
+};
+
+player.on('timeupdate', onPlay)
+console.log(onPlay)
