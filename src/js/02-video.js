24a55165cdf1aa32323121c16f9fa3fc0e4a2e57
VladIT26 <vlad.ua26@gmail.com>
2022-06-12 18:31:52 +0300

    changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,33 @@
 import Vimeo from '@vimeo/player';
-import { throttle } from 'lodash';
+import throttle from 'lodash.throttle';
+
+const VIDEO_CURRENT_TIME = "videoplayer-current-time";
+
 
 const iframe = document.querySelector('iframe');
-console.log(iframe)
 const player = new Vimeo(iframe);
 
 
-const onPlay = function (data) {
-duration: 61.857
-percent: 0.049
-seconds: 3.034
+function onPlay(event) {
+localStorage.setItem(VIDEO_CURRENT_TIME, JSON.stringify(event))
 };
 
-player.on('timeupdate', onPlay)
-console.log(onPlay)
+player.on('timeupdate', throttle(onPlay, 1000))
+
+function playerRestart() {
+    const savedTime = localStorage.getItem(VIDEO_CURRENT_TIME)
+    const parsedTime = JSON.parse(savedTime)
+    if (savedTime) {
+        player.setCurrentTime(parsedTime.seconds).then(value => {
+        console.log(value);
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
+    }
+}
+playerRestart()
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from "lodash.throttle";
 
+const formData = {};
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('[name="email"]'),
+    message: document.querySelector('[name="message"]'),
+}
+
+const FEEDBACK_FORM_STATE = 'feedback-form-state';
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onSubmitForm)
+
+getUserData()
+setUserData()
+
+function getUserData() {
+  if (localStorage.getItem(FEEDBACK_FORM_STATE)) {
+    const saveData = JSON.parse(localStorage.getItem(FEEDBACK_FORM_STATE));
+    refs.email.value = storageData.email;
+    refs.message.value = storageData.message;
+  }
+}
+
+function setUserData() {
+    formData.email = refs.email.value;
+    formData.message = refs.message.value;
+}
+
+function onFormInput(event) {
+    formData[event.target.name] = event.target.value;
+    console.log(formData[event.target.name])
+  localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(formData));   
+}
+
+
+function onSubmitForm(event) { 
+    event.preventDefault();
+    if (formData.email !== '' && formData.message !== '') {
+        console.log(formData);
+    event.currentTarget.reset();
+    localStorage.removeItem(FEEDBACK_FORM_STATE);
+    setUserData();
+  }
+}
